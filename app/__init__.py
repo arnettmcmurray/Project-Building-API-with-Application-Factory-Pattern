6bbfc0210dbb31a3b43429a9d07a6aeb98f2b9e9
Arnett McMurray <arnettmcmurray@gmail.com>
2025-10-18 22:45:56 -0500

    aligning db route for render

--- app/__init__.py
+++ app/__init__.py
@@ -40,9 +40,9 @@ def create_app(config_class=None):
             "https://mechanics-api.onrender.com",
             "https://react-mechanic-api.onrender.com",
             "http://127.0.0.1:5000",
-            "http://localhost:5000"
+            "http://localhost:5000",
             "http://127.0.0.1:5173",
-            "http://localhost:5173"
+            "http://localhost:5173"git statu
         ]}},
         supports_credentials=True
     )
